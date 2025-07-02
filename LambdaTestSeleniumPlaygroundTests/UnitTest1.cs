95b946d300e636fc2dc37844f2dd7965a4c44d83
DineshDabbakatla <Dinesh.91u@gmail.com>
2025-07-02 16:27:46 +0530

    nunit

---
+++ LambdaTestSeleniumPlaygroundTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace LambdaTestSeleniumPlaygroundTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
